60ab2fd088b4b559c047c8d3bcfc87068d70b0f2
NanaBanana98 <50379236+NanaBanana98@users.noreply.github.com>
2021-02-20 02:15:51 -0500

    Match Tan.calculate to RadianMeasureVersion3_exceptionThrow
    Discrepancy in Tan changed to match Ang's repository

--- src/tangent/Tan.java
+++ src/tangent/Tan.java
@@ -4,16 +4,30 @@ import general.*;
 
 public class Tan {
 
-	public static double calculate(IAngleMeasurement a)
+	public static double calculate(IAngleMeasurement A)
 	{
-	       double[] bernoulli=new double[10];
-	       bernoulli=Maths.Bernoulli();
-	       double temp;
-	       double sum=0;
-	       for(int i=1;i<=10;i++) {
-	           temp=Maths.power(2,2*i)*(Maths.power(2,2*i)-1)*bernoulli[i]*Maths.power(a.getRadianMeasurement(),2*i-1)/Maths.factorial(i);
-	           sum=sum+temp;
-	       }
-	       return sum;
+		try {
+        	double a = A.quadrantShift();
+        	if(a==Maths.PI/2) {
+        		throw new Exception();  //HalfPiInputForTanException
+        	}
+        	double[] bernoulli=new double[10];
+        	bernoulli=Maths.Bernoulli();
+        	double temp;
+        	double sum=0;
+        	for(int i=1;i<=10;i++) {
+        		temp=Maths.power(2,2*i)*(Maths.power(2,2*i)-1)*bernoulli[i]*Maths.power(a,2*i-1)/Maths.factorial(i);
+        		sum=sum+temp;
+        	}
+        	Quadrant q = A.getQuadrant();
+        	if(q== Quadrant.Q2 || q== Quadrant.Q4) {
+        		sum=0-sum;
+        	}
+        	return sum;
+    	}
+    	catch(Exception e){
+    		System.out.printf("Input cannot be PI/2 or 3*PI/2 \n");
+    		return -1;
+    	}
 	}
 }
