b0f2b9d0569612992a22eae5d44e330bc74bdfde
IceIceIceDB <47207173+IceIceIceDB@users.noreply.github.com>
2021-02-22 14:57:02 +0800

    Update Test_Tan.java

--- src/tangent/Test_Tan.java
+++ src/tangent/Test_Tan.java
@@ -1,5 +1,3 @@
-package tangent;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,31 +17,31 @@ class Tan_Test {
 	@Test
 	public void radCartesianBoundryTestWithZero() {
 		IAngleMeasurement radian = new RadianMeasurement(0);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void radCartesianBoundryTestWithP() {
 		IAngleMeasurement radian = new RadianMeasurement(Maths.PI);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void radCartesianBoundryTestWithDoubleP() {
 		IAngleMeasurement radian = new RadianMeasurement(Maths.PI*2);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void radNegativeBoundryTestWithP() {
 		IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void radNegativeBoundryTestWithDoubleP() {
 		IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*2);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	// Deg Test
@@ -51,25 +49,25 @@ class Tan_Test {
 	@Test
 	public void degCartesianBoundryTestWith180() {
 		IAngleMeasurement radian = new DegreeMeasurement(180);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void degCartesianBoundryTestWith360() {
 		IAngleMeasurement radian = new DegreeMeasurement(360);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void degNegativeBoundryTestWith180() {
 		IAngleMeasurement radian = new DegreeMeasurement(-180);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void degNegativeBoundryTestWith360() {
 		IAngleMeasurement radian = new DegreeMeasurement(-360);
-		Assert.assertEquals(0.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(0.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	/***
@@ -83,13 +81,13 @@ class Tan_Test {
 	@Test
 	public void degTestPositiveAngleGreaterthan360() {
 		IAngleMeasurement radian = new DegreeMeasurement(405);
-		Assert.assertEquals(1.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(1.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void degTestNegtiveAngleLessthan360() {
 		IAngleMeasurement radian = new DegreeMeasurement(-405);
-		Assert.assertEquals(-1.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(-1.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	// Deg Test
@@ -97,13 +95,13 @@ class Tan_Test {
 	@Test
 	public void radTestPositiveAngleGreaterthan2PI() {
 		IAngleMeasurement radian = new RadianMeasurement(Maths.PI*9/4);
-		Assert.assertEquals(1.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(1.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	@Test
 	public void radTestNegtiveAngleLessthan2PI() {
 		IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*9/4);
-		Assert.assertEquals(-1.0,Tan.calculate(radian),0.00001);
+		Assert.assertEquals(-1.0,Tan_FinalVersion.calculate(radian),0.00001);
 	}
 	
 	
@@ -118,25 +116,25 @@ class Tan_Test {
 	@Test
 	public void degAsymptoteTest90() {
 		IAngleMeasurement radian = new DegreeMeasurement(90);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void degAsymptoteTest270() {
 		IAngleMeasurement radian = new DegreeMeasurement(270);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void degNegativeAsymptoteTest90() {
 		IAngleMeasurement radian = new DegreeMeasurement(-90);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void degNegativeAsymptoteTest270() {
 		IAngleMeasurement radian = new DegreeMeasurement(-270);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	// Rad Test
@@ -144,24 +142,24 @@ class Tan_Test {
 	@Test
 	public void radAsymptoteTestHalfPI() {
 		IAngleMeasurement radian = new RadianMeasurement(Maths.PI/2);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void radAsymptoteTesttwoThirdsPI() {
 		IAngleMeasurement radian = new RadianMeasurement(Maths.PI*3/2);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void radNegativeAsymptoteTesttwoHalfPI() {
 		IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI/2);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 	
 	@Test
 	public void radNegativeAsymptoteTesttwoThirdsPI() {
 		IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*3/2);
-		Assertions.assertThrows(ArithmeticException.class, () -> Tan.calculate(radian));
+		Assertions.assertThrows(ArithmeticException.class, () -> Tan_FinalVersion.calculate(radian));
 	}
 }
