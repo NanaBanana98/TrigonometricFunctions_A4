07c66fe3a53ce1025bae8bb0b6a6087ddbe7e4b5
IceIceIceDB <47207173+IceIceIceDB@users.noreply.github.com>
2021-02-24 11:21:37 +0800

    Update and rename Test_Cos.java to Cos_Test.java

---
+++ src/cosine/Cos_Test.java
@@ -0,0 +1,349 @@
+package cosine;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import general.*;
+
+class Cos_Test {
+
+	/***
+	 * 
+	 * BoundryTest
+	 * 
+	 */
+	
+	@Nested
+	@DisplayName("Boundary Test")
+	class BoundryTest{
+		
+		@Nested
+		@DisplayName("Radian Test")
+		class RadianBoundryTest{
+			
+			@Nested
+			@DisplayName("Postive Test")
+			class PostiveRadianBoundryTest{
+				
+				@Test
+				public void TestWithZero() {
+					IAngleMeasurement radian = new RadianMeasurement(0);
+					Assert.assertEquals(1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWithHalfPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI/2);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWithPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI);
+					Assert.assertEquals(-1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWithTwoThirdPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI*3/2);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWithDoublePI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI*2);
+					Assert.assertEquals(1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithZero() {
+					IAngleMeasurement radian = new RadianMeasurement(0.1);
+					Assert.assertEquals(0.995004,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithHalfPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI/2+0.1);
+					Assert.assertEquals(-0.099833,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI+0.1);
+					Assert.assertEquals(-0.995004,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithTwoThirdPI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI*3/2+0.1);
+					Assert.assertEquals(0.099833,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithDoublePI() {
+					IAngleMeasurement radian = new RadianMeasurement(Maths.PI*2+0.1);
+					Assert.assertEquals(0.995004,Cos_FinalVersion.calculate(radian),0.000001);
+				}				
+				
+			}
+			
+			@Nested
+			@DisplayName("Negative Test")
+			class NegativeRadianBoundryTest{
+				
+				@Test
+				public void radNegativeBoundryTestWithHalfPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI/2);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void radNegativeBoundryTestWithPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI);
+					Assert.assertEquals(-1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void radNegativeBoundryTestWithTwoThirdPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*3/2);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void radNegativeBoundryTestWithDoublePI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*2);
+					Assert.assertEquals(1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+								
+				@Test
+				public void JustAboveTestWithHalfPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI/2+0.1);
+					Assert.assertEquals(0.099833,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI+0.1);
+					Assert.assertEquals(-0.995004,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithTwoThirdPI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*3/2+0.1);
+					Assert.assertEquals(-0.099833,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWithDoublePI() {
+					IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*2+0.1);
+					Assert.assertEquals(0.995004,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+											
+			}
+					
+		}
+		
+		@Nested
+		@DisplayName("Degree Test")
+		class DegreeBoundryTest{
+			
+			@Nested
+			@DisplayName("Postive Test")
+			class PostiveRadianBoundryTest{
+				
+				@Test
+				public void TestWith90() {
+					IAngleMeasurement radian = new DegreeMeasurement(90);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith180() {
+					IAngleMeasurement radian = new DegreeMeasurement(180);
+					Assert.assertEquals(-1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith270() {
+					IAngleMeasurement radian = new DegreeMeasurement(270);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith360() {
+					IAngleMeasurement radian = new DegreeMeasurement(360);
+					Assert.assertEquals(1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith90() {
+					IAngleMeasurement radian = new DegreeMeasurement(90.1);
+					Assert.assertEquals(-0.001745,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith180() {
+					IAngleMeasurement radian = new DegreeMeasurement(180.1);
+					Assert.assertEquals(-0.999998,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith270() {
+					IAngleMeasurement radian = new DegreeMeasurement(270.1);
+					Assert.assertEquals(0.001745,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith360() {
+					IAngleMeasurement radian = new DegreeMeasurement(360.1);
+					Assert.assertEquals(0.999998,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				
+				
+			}
+			
+			@Nested
+			@DisplayName("Negative Test")
+			class NegativeRadianBoundryTest{
+				
+				@Test
+				public void TestWith90() {
+					IAngleMeasurement radian = new DegreeMeasurement(-90);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith180() {
+					IAngleMeasurement radian = new DegreeMeasurement(-180);
+					Assert.assertEquals(-1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith270() {
+					IAngleMeasurement radian = new DegreeMeasurement(-270);
+					Assert.assertEquals(0.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void TestWith360() {
+					IAngleMeasurement radian = new DegreeMeasurement(-360);
+					Assert.assertEquals(1.0,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith90() {
+					IAngleMeasurement radian = new DegreeMeasurement(-89.9);
+					Assert.assertEquals(0.001745,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith180() {
+					IAngleMeasurement radian = new DegreeMeasurement(-179.9);
+					Assert.assertEquals(-0.999998,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith270() {
+					IAngleMeasurement radian = new DegreeMeasurement(-269.9);
+					Assert.assertEquals(-0.001745,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+				@Test
+				public void JustAboveTestWith360() {
+					IAngleMeasurement radian = new DegreeMeasurement(-359.9);
+					Assert.assertEquals(0.999998,Cos_FinalVersion.calculate(radian),0.000001);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	/***
+	 * 
+	 * Greater than 360 test. 
+	 * 
+	 */
+
+	@Nested
+	@DisplayName("Out of 360 Test")
+	class OutOf360Test{
+		
+		@Nested
+		@DisplayName("Radian Test")
+		class RadianOutOf360Test{
+			
+			@Test
+			public void TestGreaterthan2PI() {
+				IAngleMeasurement radian = new RadianMeasurement(Maths.PI*7/3);
+				Assert.assertEquals(0.5,Cos_FinalVersion.calculate(radian),0.000001);
+			}
+			
+			@Test
+			public void TestLessthanNegative2PI() {
+				IAngleMeasurement radian = new RadianMeasurement((-1)*Maths.PI*7/3);
+				Assert.assertEquals(0.5,Cos_FinalVersion.calculate(radian),0.000001);
+			}
+			
+		}
+		
+		@Nested
+		@DisplayName("Degree Test")
+		class DegreeOutOf360Test{
+			
+			@Test
+			public void TestGreaterthan360() {
+				IAngleMeasurement radian = new DegreeMeasurement(420);
+				Assert.assertEquals(0.5,Cos_FinalVersion.calculate(radian),0.000001);
+			}
+			
+			@Test
+			public void TestLessthanNegative360() {
+				IAngleMeasurement radian = new DegreeMeasurement(-420);
+				Assert.assertEquals(0.5,Cos_FinalVersion.calculate(radian),0.000001);
+			}
+			
+		}
+	}
+	
+	/***
+	 * 
+	 * Whole Equivalence Partition Test
+	 * 
+	 */
+	
+	@Nested
+	@DisplayName("Whole Equivalence Partition Test")
+	class WholeEquivalencePartitionTest{
+		
+		@Test
+		public void TestWith45() {
+			IAngleMeasurement radian = new DegreeMeasurement(45);
+			Assert.assertEquals(0.707107,Cos_FinalVersion.calculate(radian),0.000001);
+		}
+		
+		@Test
+		public void TestWith135() {
+			IAngleMeasurement radian = new DegreeMeasurement(135);
+			Assert.assertEquals(-0.707107,Cos_FinalVersion.calculate(radian),0.000001);
+		}
+		
+		@Test
+		public void TestWith225() {
+			IAngleMeasurement radian = new DegreeMeasurement(225);
+			Assert.assertEquals(-0.707107,Cos_FinalVersion.calculate(radian),0.000001);
+		}
+		
+		@Test
+		public void TestWith315() {
+			IAngleMeasurement radian = new DegreeMeasurement(315);
+			Assert.assertEquals(0.707107,Cos_FinalVersion.calculate(radian),0.000001);
+		}
+		
+	}
+		
+}
