5463d946fe2f595865e0738eb3bde0269c263b09
IceIceIceDB <47207173+IceIceIceDB@users.noreply.github.com>
2021-02-24 11:22:31 +0800

    Update and rename Cos.java to Cos_FinalVersion.java

--- src/cosine/Cos_FinalVersion.java
+++ src/cosine/Cos_FinalVersion.java
@@ -2,16 +2,18 @@ package cosine;
 
 import general.*;
 
-public class Cos {
+public class Cos_FinalVersion {
 
 	public static double calculate(IAngleMeasurement a) {
-	       double sum=0,temp=1;
-	       for(int i = 2; i<=20; i=i+2) {
+	       double sum=1.0,temp=1.0;
+	       for(int i = 2; i<=28; i=i+2) {
 	           for(int j=i;j>0;j--) {
 	               temp=temp*a.getRadianMeasurement()/j;
 	           }
-	           sum=sum+temp;
+	           sum=sum+Maths.power(-1.0,i/2)*temp;
+	           temp=1.0;
 	       }
 	       return sum;
 	}
 }
+
